672d6b19cc8b4e2a7d7afe795e33bcb591b24ae4
PhucLee14 <lhphucth14@gmail.com>
2024-03-16 12:24:26 +0700

    add table data

--- frontend/src/components/Layout/components/Table/index.jsx
+++ frontend/src/components/Layout/components/Table/index.jsx
@@ -1,20 +1,19 @@
 import * as React from "react";
 import { useTable } from "react-table";
 import styles from "./Table.module.scss";
-import fakeData from "~/MOCK_DATA.json";
-import fakeDataDrl from "~/drl1.json";
+import drlData from "~/DIEM_REN_LUYEN.json";
 import Button from "../Button";
 import classNames from "classnames/bind";
 
 const cx = classNames.bind(styles);
-
+var i = 1;
 function Table({ dataName }) {
     const data = React.useMemo(() => dataName, []);
     const columns = React.useMemo(
         () => [
             {
                 Header: "STT",
-                accessor: "id",
+                accessor: "ID",
             },
             {
                 Header: "Nội dung",
@@ -30,6 +29,7 @@ function Table({ dataName }) {
 
     const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
         useTable({ columns, data });
+    console.log(data);
 
     return (
         <div className={cx("container")}>
@@ -47,17 +47,108 @@ function Table({ dataName }) {
                     ))}
                 </thead>
                 <tbody {...getTableBodyProps()}>
-                    {rows.map((row) => {
+                    {rows.map((row, index) => {
                         prepareRow(row);
                         return (
                             <tr {...row.getRowProps()}>
                                 {row.cells.map((cell) => (
                                     <td {...cell.getCellProps()}>
-                                        {" "}
-                                        {cell.render("Cell")}
-                                        <p>
-                                            {console.log(cell.getCellProps())}
-                                        </p>
+                                        {cell
+                                            .getCellProps()
+                                            .key.substring(
+                                                cell.getCellProps().key.length -
+                                                    7
+                                            ) === "content"
+                                            ? cell
+                                                  .render("Cell")
+                                                  .props.data[
+                                                      index
+                                                  ].content.split("\n")[0]
+                                            : cell.render("Cell")}
+                                        {cell
+                                            .getCellProps()
+                                            .key.substring(
+                                                cell.getCellProps().key.length -
+                                                    7
+                                            ) === "content" ? (
+                                            <div
+                                                className={cx("activity-type")}
+                                            >
+                                                {cell
+                                                    .render("Cell")
+                                                    .props.data[
+                                                        index
+                                                    ].content.split("\n")
+                                                    .map((item, i) =>
+                                                        cell
+                                                            .render("Cell")
+                                                            .props.data[
+                                                                index
+                                                            ].content.split(
+                                                                "\n"
+                                                            )[i] !=
+                                                            cell
+                                                                .render("Cell")
+                                                                .props.data[
+                                                                    index
+                                                                ].content.split(
+                                                                    "\n"
+                                                                )[0] &&
+                                                        cell
+                                                            .render("Cell")
+                                                            .props.data[
+                                                                index
+                                                            ].content.split(
+                                                                "\n"
+                                                            )[i] !=
+                                                            cell
+                                                                .render("Cell")
+                                                                .props.data[
+                                                                    index
+                                                                ].content.split(
+                                                                    "\n"
+                                                                )[
+                                                                cell
+                                                                    .render(
+                                                                        "Cell"
+                                                                    )
+                                                                    .props.data[
+                                                                        index
+                                                                    ].content.split(
+                                                                        "\n"
+                                                                    ).length - 1
+                                                            ] ? (
+                                                            <p
+                                                                className={cx(
+                                                                    "activity-detail"
+                                                                )}
+                                                            >
+                                                                {item}
+                                                            </p>
+                                                        ) : (
+                                                            <p></p>
+                                                        )
+                                                    )}
+                                                {
+                                                    cell
+                                                        .render("Cell")
+                                                        .props.data[
+                                                            index
+                                                        ].content.split("\n")[
+                                                        cell
+                                                            .render("Cell")
+                                                            .props.data[
+                                                                index
+                                                            ].content.split(
+                                                                "\n"
+                                                            ).length - 1
+                                                    ]
+                                                }
+                                            </div>
+                                        ) : (
+                                            ""
+                                        )}
+                                        {}
                                     </td>
                                 ))}
                                 <td>
--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,16 +1,12 @@
 import Table from "~/components/Layout/components/Table";
 import Button from "~/components/Layout/components/Button";
-import fakeData from "~/MOCK_DATA.json";
-import fakeDataDrl from "~/drl.json";
-import fakeDataDrl1 from "~/drl1.json";
-import fakeDataDrl2 from "~/drl2.json";
+import drlData from "~/DIEM_REN_LUYEN.json";
 import classNames from "classnames/bind";
 import styles from "./Home.module.scss";
 
 const cx = classNames.bind(styles);
 
 function Home() {
-    console.log(fakeDataDrl);
     return (
         <div className={cx("home-container")}>
             <div className={cx("controll-section")}>
@@ -30,10 +26,7 @@ function Home() {
                 />
                 <Button primary>Check</Button>
             </div>
-            {/* <h3 className={cx("table-type")}>
-                1. Đánh giá về ý thức tham gia học tập (Điểm tối đa 20 điểm)
-            </h3> */}
-            <Table dataName={fakeDataDrl} />
+            <Table dataName={drlData} />
         </div>
     );
 }
